588b6eaebfeae455c5e53b08521f8558b0c7a717
Rahul-NITD <211220042@nitdelhi.ac.in>
2024-03-09 02:47:18 +0530

    refactored main.py

--- main.py
+++ main.py
@@ -11,18 +11,20 @@ def home_path():
 
 @app.route("/curl", methods=["GET", "POST"])
 def curl_path():
-    args = request.args
-    headers = json.loads(args.get("headers", "{}"))
-    method = str.upper(args.get("method", "GET")) 
-    if method not in ["GET", "POST"]:
-        return Response(response='{"error": "method not supported"}', status=HTTPStatus.METHOD_NOT_ALLOWED)
+    try:
+        args = request.args
+        headers = {**dict(request.headers) , **json.loads(args.get("headers", "{}"))}
+        method = str.upper(args.get("method", "GET")) 
+        if method not in ["GET", "POST"]:
+            return Response(response='{"error": "method not supported"}', status=HTTPStatus.METHOD_NOT_ALLOWED)
 
-    print("Here only!")
-    if "req" not in args:
-        return Response(status=HTTPStatus.NO_CONTENT)
+        if "req" not in args:
+            return Response(status=HTTPStatus.NO_CONTENT)
             
-    res = requests.request(method=method,url=args["req"], headers=headers, data=request.data)
-    return res.text
+        res = requests.request(method=method,url=args["req"], headers=headers, data=request.data)
+        return res.text
+    except Exception as e:
+        return Response(json.dumps(str(e)), status=HTTPStatus.INTERNAL_SERVER_ERROR)
 
 if __name__ == "__main__":
     app.run(host="localhost", port=3000, debug=True)
